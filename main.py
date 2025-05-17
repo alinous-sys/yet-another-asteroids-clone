c3734c7e7c2a08e294e69afd022a512a0fdd467a
ftancula <ftancula@gmail.com>
2025-05-17 16:12:28 +0200

    refactor into pygame groups

--- main.py
+++ main.py
@@ -6,19 +6,26 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
     clock = pygame.time.Clock()
     dt = 0
     x = SCREEN_WIDTH/2
     y = SCREEN_HEIGHT/2
+
+    Player.containers = (updatable,drawable)
     player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
 
